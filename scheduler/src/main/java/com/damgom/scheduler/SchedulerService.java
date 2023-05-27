f2fc118fc919117deac836e5f4c6deea4fd785d2
Damgom <wisewish1@gmail.com>
2023-05-27 14:13:35 +0900

    schedule initial delay

--- scheduler/src/main/java/com/damgom/scheduler/SchedulerService.java
+++ scheduler/src/main/java/com/damgom/scheduler/SchedulerService.java
@@ -9,6 +9,8 @@ public class SchedulerService {
     /**
      * Spring Bean 으로 등록된 클래스만 가능
      * Parameter 사용 불가, return Type 은 void
+     *
+     * @ScheduleFixedDelay
      * fixedDelay 는 milliseconds 단위, 이전 Task 종료 시점부터 정의된 시간만큼 지난 후 Task 를 실행한다.
      */
 //    @Scheduled(fixedDelay = 1000)
@@ -18,11 +20,23 @@ public class SchedulerService {
 //    }
 
     /**
+     * @ScheduleFixedRate
      * 이전 task 시작 시점부터 정의된 시간만큼 지난 후 task 를 실행.
      */
-    @Scheduled(fixedRate = 1000)
-    public void scheduleFixedRate() {
+//    @Scheduled(fixedRate = 1000)
+//    public void scheduleFixedRate() {
+//        System.out.println(
+//                "Fixed rate task : " + System.currentTimeMillis() / 1000);
+//    }
+
+    /**
+     * @ScheduleInitialDelay
+     * 초기지연시간을 설정한다. ex. initialDelay 가 5000인 경우 메소드가 등록되고 5초 후 정해진 시간마다 메소드 실행
+     */
+    @Scheduled(fixedDelay = 3000, initialDelay = 5000)
+    public void scheduleInitialDelay() {
         System.out.println(
-                "Fixed rate task : " + System.currentTimeMillis() / 1000);
+                "InitialDelay : " + System.currentTimeMillis() / 1000);
     }
+
 }
